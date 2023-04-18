53f25a5dcd7ad2f99212d20929501a969c00828c
p6nj <breee@duck.com>
2023-04-18 16:54:25 +0200

    some classes

--- pygraph.py
+++ pygraph.py
@@ -1,9 +1,38 @@
 from tkinter import Tk, Canvas
-from math import sin
+from math import sin, cos, pi
+size = 600
+
+
+class Place:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+
+class Point:
+    def __init__(self, place: Place) -> None:
+        self.place = place
+
+
+class Graph:
+    def __init__(self, order: int) -> None:
+        self.order = order
+        self.points = [Place(((size/2)*sin(2*pi*i/order)+size/2+5,
+                              (size/2)*cos(2*pi*i/order)+size/2+5)*2) for i in range(i)]
+
+    def show(self):
+        w = Tk()
+        can = Canvas(w, width=size+10, height=size+10)
+        can.pack()
+        for i in range(steps):
+            can.create_rectangle(((size/2)*sin(2*pi*i/steps)+size/2+5,
+                                  (size/2)*cos(2*pi*i/steps)+size/2+5)*2)
+
+
 w = Tk()
-can = Canvas(w,width=600,height=600)
+can = Canvas(w, width=size+10, height=size+10)
 can.pack()
-a=300
-for i in range(10):
-    can.create_rectangle((sin(i),3)*2)
+for i in range(steps):
+    can.create_rectangle(((size/2)*sin(2*pi*i/steps)+size/2+5,
+                         (size/2)*cos(2*pi*i/steps)+size/2+5)*2)
 w.mainloop()
